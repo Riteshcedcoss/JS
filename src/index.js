a63d7369953ff21b1f23e1b04f196682f509e7dd
Riteshcedcoss <riteshkumargupta@cedcoss.com>
2022-02-05 13:17:48 +0530

    Use Bracket Notation to Find the First Character in a Strin

--- src/index.js
+++ src/index.js
@@ -119,9 +119,16 @@ Welcome to Cedcoss
 // let myStr = "Learning to code is ";
 // myStr +=someAdjective;
 
+// // Setup
+// let lastNameLength = 0;
+// const lastName = "Lovelace";
+
+// // Only change code below this line
+// lastNameLength = lastName.length;
+
 // Setup
-let lastNameLength = 0;
+let firstLetterOfLastName = "";
 const lastName = "Lovelace";
 
 // Only change code below this line
-lastNameLength = lastName.length;
+firstLetterOfLastName = lastName[0]; // Change this line
