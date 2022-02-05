2ee8a3a86e0d4957cd9e2928f09a078d60bad075
Riteshcedcoss <riteshkumargupta@cedcoss.com>
2022-02-05 10:49:37 +0530

    Understanding Uninitialized Variables

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@
 /*
 Welcome to Cedcoss
 */
-var myName;
-// Setup
-var a;
-a = 7;
-var b;
+// var myName;
+// // Setup
+// var a;
+// a = 7;
+// var b;
 
-// Only change code below this line
-b=a;
-var c=9;
+// // Only change code below this line
+// b=a;
+// var c=9;
 
 var myFirstName="Ritesh";
 var myLastName="gupta";
+// Only change code below this line
+var a=5;
+var b=10;
+var c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
