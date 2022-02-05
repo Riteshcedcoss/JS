e188ab13bc6f083203c458b8e491dc0b47d352d3
Riteshcedcoss <riteshkumargupta@cedcoss.com>
2022-02-05 14:03:39 +0530

    Access Multi-Dimensional Arrays With Indexes

--- src/index.js
+++ src/index.js
@@ -175,7 +175,16 @@ const lastName = "Lovelace";
 // // Only change code below this line.
 // var myData = myArray[0];
 // Setup
-const myArray = [18, 64, 99];
+// const myArray = [18, 64, 99];
 
-// Only change code below this line
-myArray[0] = 45;
+// // Only change code below this line
+// myArray[0] = 45;
+
+const myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData = myArray[2][1];
