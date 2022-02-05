2177a572ead5f6da8b379761b337a47a37d7c751
Riteshcedcoss <riteshkumargupta@cedcoss.com>
2022-02-05 15:49:48 +0530

    Understanding Boolean Values

--- src/index.js
+++ src/index.js
@@ -290,18 +290,22 @@ function fun1() {
 // // Only change code below this line
 // processed = processArg(7);
 
-function nextInLine(arr, item) {
+// function nextInLine(arr, item) {
+//     // Only change code below this line
+//     arr.push(item);
+//     var firstItem = arr.shift(arr);
+//     return firstItem;
+//     // Only change code above this line
+//   }
+  
+//   // Setup
+//   const testArr = [1, 2, 3, 4, 5];
+  
+//   // Display code
+//   console.log("Before: " + JSON.stringify(testArr));
+//   console.log(nextInLine(testArr, 6));
+//   console.log("After: " + JSON.stringify(testArr));
+function welcomeToBooleans() {
     // Only change code below this line
-    arr.push(item);
-    var firstItem = arr.shift(arr);
-    return firstItem;
-    // Only change code above this line
-  }
   
-  // Setup
-  const testArr = [1, 2, 3, 4, 5];
-  
-  // Display code
-  console.log("Before: " + JSON.stringify(testArr));
-  console.log(nextInLine(testArr, 6));
-  console.log("After: " + JSON.stringify(testArr));
+    return true;
