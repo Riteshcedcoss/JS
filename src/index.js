2a513b274044335271a062200f114e70e372c8ae
Riteshcedcoss <riteshkumargupta@cedcoss.com>
2022-02-05 12:17:19 +0530

    Escape Sequences in Strings

--- src/index.js
+++ src/index.js
@@ -12,98 +12,98 @@ Welcome to Cedcoss
 // b=a;
 // var c=9;
 
-var myFirstName="Ritesh";
-var myLastName="gupta";
-// Only change code below this line
-var a=5;
-var b=10;
-var c="I am a";
-// Only change code above this line
+// var myFirstName="Ritesh";
+// var myLastName="gupta";
+// // Only change code below this line
+// var a=5;
+// var b=10;
+// var c="I am a";
+// // Only change code above this line
 
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
 
-// Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
+// // Variable declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
 
-// Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver= 9000;
+// // Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver= 9000;
 
-let catName = "Oliver";
-let catSound = "Meow!";
+// let catName = "Oliver";
+// let catSound = "Meow!";
 
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
-fact = "is awesome!";
-console.log(FCC, fact); // Change this line
+// const FCC = "freeCodeCamp"; // Change this line
+// let fact = "is cool!"; // Change this line
+// fact = "is awesome!";
+// console.log(FCC, fact); // Change this line
 
-const sum = 10 + 10;
-const difference = 45 - 33;
-const product = 8 * 10;
-const quotient = 66 / 33;
+// const sum = 10 + 10;
+// const difference = 45 - 33;
+// const product = 8 * 10;
+// const quotient = 66 / 33;
 
-let myVar = 87;
+// let myVar = 87;
 
-// Only change code below this line
+// // Only change code below this line
 
-myVar++;
+// myVar++;
 
-let myVar = 11;
+// let myVar = 11;
 
-// Only change code below this line
-myVar --;
+// // Only change code below this line
+// myVar --;
 
-const ourDecimal = 5.7;
+// const ourDecimal = 5.7;
 
-// Only change code below this line
-let myDecimal = 5.7;
+// // Only change code below this line
+// let myDecimal = 5.7;
 
-const product = 2.0 * 2.5;
+// const product = 2.0 * 2.5;
 
-const quotient = 4.4 / 2.0; // Change this line
+// const quotient = 4.4 / 2.0; // Change this line
 
-const remainder = 11 % 3;
+// const remainder = 11 % 3;
 
-let a = 3;
-let b = 17;
-let c = 12;
+// let a = 3;
+// let b = 17;
+// let c = 12;
 
-// Only change code below this line
-a+=12;
-b+=9;
-c+=7;
+// // Only change code below this line
+// a+=12;
+// b+=9;
+// c+=7;
 
-let a = 11;
-let b = 9;
-let c = 3;
+// let a = 11;
+// let b = 9;
+// let c = 3;
 
-// Only change code below this line
-a-=6;
-b-=15;
-c-=1;
+// // Only change code below this line
+// a-=6;
+// b-=15;
+// c-=1;
 
-let a = 5;
-let b = 12;
-let c = 4.6;
+// let a = 5;
+// let b = 12;
+// let c = 4.6;
 
-// Only change code below this line
-a*=5;
-b*=3;
-c*=10;
+// // Only change code below this line
+// a*=5;
+// b*=3;
+// c*=10;
 
-let a = 48;
-let b = 108;
-let c = 33;
+// let a = 48;
+// let b = 108;
+// let c = 33;
 
-// Only change code below this line
-a/=12;
-b/=4;
-c/=11;
+// // Only change code below this line
+// a/=12;
+// b/=4;
+// c/=11;
 
 const myStr ="I am a \"double quoted\" string inside \"double quotes\"."// Change this line
 
