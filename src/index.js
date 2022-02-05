d32aba552f72b2450a540b15f1e985a7762adf76
Riteshcedcoss <riteshkumargupta@cedcoss.com>
2022-02-05 13:44:06 +0530

    Word Blanks

--- src/index.js
+++ src/index.js
@@ -154,3 +154,11 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const secondToLastLetterOfLastName = lastName[lastName.length -2]; // Change this line
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+let wordBlanks = "Once there was a " + myNoun + " which was very " + myAdjective + ". ";
+wordBlanks += "It " + myVerb + " " + myAdverb + " around the yard.";
